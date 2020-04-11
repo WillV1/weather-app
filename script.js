2002d1dc9a5ca0d5f9f9d9b29d19008869d1cd3f
WillV1 <wmvanhook@gmail.com>
2020-04-11 07:45:52 -0400

    Updating for card styling

---
+++ script.js
@@ -0,0 +1,24 @@
+// Create html/use bootstrap for including 
+//nested columns for 5 day forecast/ card for current city weather element; 
+//nested rows(?) or table for previously visited cities
+//header for "Weather Dashboard"
+
+//Install bootstrap for additional styling 
+
+//Install jQuery for JS code generation functionality
+
+
+
+//Generate search button for city input
+
+//Set up click event that when a user searches for a city, a city is able to be pulled and viewed from search box
+
+//Set up array for previously visited cities
+
+//Set up click event for when user clicks on previous cities to pull up forecast
+
+//Set up Open Weather API to connect weather data to page
+
+//Generate code for storage of current information in information card; 5 day forecast information; on different urls
+
+//Set up local storage to store persistent changes in array of previously visited cities
